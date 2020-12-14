f44d71a11d7f1ad6581b8c49ee2983911b5e55e9
De Zaldua Samuel <dezalduasamuel@MacBook-Pro-de-De.local>
2020-12-14 15:18:34 +0100

    moved authenticate func in event listener

--- index.js
+++ index.js
@@ -9,19 +9,19 @@ const doc = new GoogleSpreadsheet(process.env.DOC);
 // LOGIN
 client.login(process.env.DISCORD_LOGIN);
 
-// const authenticate = async () => {
+const authenticate = async () => {
 
-//   await doc.useServiceAccountAuth({
-//     client_email: process.env.API_SERVICE_ACCOUNT_EMAIL,
-//     private_key: process.env.PRIVATE_KEY
-//   });
+  await doc.useServiceAccountAuth({
+    client_email: process.env.API_SERVICE_ACCOUNT_EMAIL,
+    private_key: process.env.PRIVATE_KEY
+  });
   
-//   await doc.loadInfo();
-// };
-// authenticate();
+  await doc.loadInfo();
+};
 
 // BOT IS ON
 const readyDiscord = async () => {
+  await authenticate();
   console.log('Ready to work');
 }
 client.once('ready', readyDiscord);
@@ -37,14 +37,14 @@ client.on('message', msg => {
   if (msg.content.includes('https://soundcloud.com')){
     msg.reply('test')
     
-    //Extract Sdcld link from msg
-    // const trackLink = msg.content.split(/[\s]+/);
-    // const url = trackLink.filter(word => word.startsWith('https://soundcloud'));
+    // Extract Sdcld link from msg
+    const trackLink = msg.content.split(/[\s]+/);
+    const url = trackLink.filter(word => word.startsWith('https://soundcloud'));
     
-    // //Update sheet
-    // const sheet = doc.sheetsByIndex[0];
-    // const addRow = () => sheet.addRow({Name: msg.author.username, Soundcloud: String(url[0])});
-    // addRow();
+    //Update sheet
+    const sheet = doc.sheetsByIndex[0];
+    const addRow = () => sheet.addRow({Name: msg.author.username, Soundcloud: String(url[0])});
+    addRow();
   }
 })
 
